8f89a34d0519924aa7bcc16b7740f1e9351e54d3
joffitzer <joffitzer@gmail.com>
2020-03-10 10:05:47 -0400

    simple func

---
+++ pyth-funcs.py
@@ -0,0 +1,14 @@
+# def say_hi():
+#     print("Hello User")
+#
+#
+# print("Top")
+# say_hi()
+# print("Bottom")
+
+def say_hi(name, age):
+    print("Hello " + name + ", you are " + str(age) + " years old.")
+
+say_hi("Mike", 53)
+say_hi("Steve", 22)
+
